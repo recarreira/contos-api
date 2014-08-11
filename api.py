d124751721c5e4e4919f3d32f9aec3405d722b90
Renata Carreira <re.carreira@gmail.com>
2014-08-10 21:42:17 -0300

    Getting started. Trying to list stories in /stories :)

--- api.py
+++ api.py
@@ -1,13 +1,37 @@
 import tornado.ioloop
 import tornado.web
+from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.objectid import ObjectId
 
-class MainHandler(tornado.web.RequestHandler):
+MONGODB_DB_URL = 'mongodb://localhost:27017/'
+MONGODB_DB_NAME =  'stories'
+
+client = MongoClient(MONGODB_DB_URL)
+db = client[MONGODB_DB_NAME]
+
+settings = {
+    "debug" : True
+}
+
+
+class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+
+class StoriesHandler(tornado.web.RequestHandler):
+    def get(self):
+        stories = db.stories.find()
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps(list(stories),default=json_util.default, sort_keys=True, indent=4))
+
+
 application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+    (r'/', IndexHandler),
+    (r'/stories',StoriesHandler),
+],**settings)
 
 if __name__ == "__main__":
     application.listen(8888)
